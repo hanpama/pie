9628d62876d264cef430ee4946339929d34d1816
Kyungil Choi <hanpama@gmail.com>
2024-03-30 10:05:46 +0900

    Change column introspection to compose full type expression (base type + typmod + is array)

--- src/introspection/column.rs
+++ src/introspection/column.rs
@@ -1,5 +1,6 @@
 use postgres::{Error, Transaction};
 
+#[derive(Debug, PartialEq)]
 pub struct Column {
     pub schema: String,
     pub table: String,
@@ -7,12 +8,10 @@ pub struct Column {
     pub name: String,
     pub default: String,
     pub not_null: bool,
-    pub data_type_name: String,
-    pub is_array: bool,
-    pub type_mod: Option<i32>,
+    pub data_type: String,
 }
 
-fn introspect_columns(client: &mut Transaction, schemas: &[&str]) -> Result<Vec<Column>, Error> {
+pub fn introspect_columns(client: &mut Transaction, schemas: &[&str]) -> Result<Vec<Column>, Error> {
     let query = include_str!("column.sql");
     let stmt = client.prepare(query)?;
     let rows = client.query(&stmt, &[&schemas])?;
@@ -26,10 +25,9 @@ fn introspect_columns(client: &mut Transaction, schemas: &[&str]) -> Result<Vec<
             name: row.get(3),
             default: row.get(4),
             not_null: row.get(5),
-            data_type_name: row.get(6),
-            is_array: row.get(7),
-            type_mod: row.get(8),
-        };
+            data_type: row.get(6),
+        };        
+
         vals.push(val);
     }
     Ok(vals)
@@ -38,12 +36,59 @@ fn introspect_columns(client: &mut Transaction, schemas: &[&str]) -> Result<Vec<
 #[cfg(test)]
 mod tests {
     use super::introspect_columns;
-    use crate::util::test::get_test_connection;
+    use crate::{introspection::column::Column, util::test::get_test_connection};
 
     #[test]
     fn test_introspect_columns() {
         let mut conn = get_test_connection();
         let mut tx = conn.transaction().unwrap();
-        introspect_columns(&mut tx, &vec!["public"]).unwrap();
+        tx.execute("CREATE SCHEMA test_column", &[]).unwrap();
+        tx.execute("
+            CREATE TABLE test_column.table1 (
+                col_bool bool,
+                col_varchar varchar(255),
+                col_numeric numeric(13, 4),
+                col_array text[]
+            );
+        ", &[]).unwrap();
+        let res = introspect_columns(&mut tx, &vec!["test_column"]).unwrap();
+        assert_eq!(res, vec![
+            Column {
+                schema: "test_column".to_string(),
+                table: "table1".to_string(),
+                att_num: 1,
+                name: "col_bool".to_string(),
+                default: "".to_string(),
+                not_null: false,
+                data_type: "bool".to_string(),
+            },
+            Column {
+                schema: "test_column".to_string(),
+                table: "table1".to_string(),
+                att_num: 2,
+                name: "col_varchar".to_string(),
+                default: "".to_string(),
+                not_null: false,
+                data_type: "varchar(255)".to_string(),
+            },
+            Column {
+                schema: "test_column".to_string(),
+                table: "table1".to_string(),
+                att_num: 3,
+                name: "col_numeric".to_string(),
+                default: "".to_string(),
+                not_null: false,
+                data_type: "numeric(13,4)".to_string(),
+            },
+            Column {
+                schema: "test_column".to_string(),
+                table: "table1".to_string(),
+                att_num: 4,
+                name: "col_array".to_string(),
+                default: "".to_string(),
+                not_null: false,
+                data_type: "text[]".to_string(),
+            },
+        ]);
     }
 }
--- src/introspection/column.sql
+++ src/introspection/column.sql
@@ -1,36 +1,56 @@
 SELECT 
-    nc.nspname AS "schema",
-    c.relname AS "table",
-    a.attnum AS ordinal_position,
-    a.attname AS column_name,
-    COALESCE( CASE WHEN a.attgenerated = '' THEN pg_get_expr(ad.adbin, ad.adrelid) ELSE null END, '') AS column_default,
+    nc.nspname 
+        AS "schema",
+    c.relname
+        AS "table",
+    a.attnum
+        AS "ordinal_position",
+    a.attname
+        AS "column_name",
+    COALESCE( CASE WHEN a.attgenerated = '' THEN pg_get_expr(ad.adbin, ad.adrelid) ELSE null END, '')
+        AS "column_default",
+    a.attnotnull OR t.typtype = 'd' AND t.typnotnull
+        AS "not_null",
     CASE
-            WHEN a.attnotnull
-                    OR t.typtype = 'd'
-                    AND t.typnotnull THEN TRUE
-            ELSE FALSE
-    END AS not_null,
-    TRIM(LEADING '_' FROM COALESCE(bt.typname, t.typname)) as type_name,
-    CASE
-            WHEN t.typtype = 'd'
-                    AND bt.typelem <> 0
-                    AND bt.typlen = -1 THEN TRUE
-            WHEN t.typelem <> 0
-                    AND t.typlen = -1 THEN TRUE
-            ELSE FALSE
-    END AS is_array,
-    COALESCE(
-        CASE a.atttypmod WHEN -1 THEN NULL ELSE a.atttypmod END,
-        CASE t.typtypmod WHEN -1 THEN NULL ELSE t.typtypmod END
-    ) typmod
+        WHEN t.typelem <> 0 THEN (SELECT eltype.typname FROM pg_type eltype WHERE eltype.oid = t.typelem) 
+        ELSE t.typname
+    END 
+    ||
+    CASE WHEN a.atttypmod != -1 THEN
+        CASE t.typname
+                WHEN 'bpchar' THEN bpchartypmodout(a.atttypmod)
+                WHEN '_bpchar' THEN bpchartypmodout(a.atttypmod)
+                WHEN 'varchar' THEN varchartypmodout(a.atttypmod)
+                WHEN '_varchar' THEN varchartypmodout(a.atttypmod)
+                WHEN 'time' THEN timetypmodout(a.atttypmod)
+                WHEN '_time' THEN timetypmodout(a.atttypmod)
+                WHEN 'timestamp' THEN timestamptypmodout(a.atttypmod)
+                WHEN '_timestamp' THEN timestamptypmodout(a.atttypmod)
+                WHEN 'timestamptz' THEN timestamptztypmodout(a.atttypmod)
+                WHEN '_timestamptz' THEN timestamptztypmodout(a.atttypmod)
+                WHEN 'interval' THEN intervaltypmodout(a.atttypmod)
+                WHEN '_interval' THEN intervaltypmodout(a.atttypmod)
+                WHEN 'timetz' THEN timetztypmodout(a.atttypmod)
+                WHEN '_timetz' THEN timetztypmodout(a.atttypmod)
+                WHEN 'bit' THEN bittypmodout(a.atttypmod)
+                WHEN '_bit' THEN bittypmodout(a.atttypmod)
+                WHEN 'varbit' THEN varbittypmodout(a.atttypmod)
+                WHEN '_varbit' THEN varbittypmodout(a.atttypmod)
+                WHEN 'numeric' THEN numerictypmodout(a.atttypmod)
+                WHEN '_numeric' THEN numerictypmodout(a.atttypmod)
+                ELSE null
+        END::TEXT
+    ELSE ''
+    END
+    ||
+    CASE WHEN t.typelem <> 0 THEN '[]' ELSE '' END
+        AS "data_type"
 FROM pg_attribute a
     LEFT JOIN pg_attrdef ad ON a.attrelid = ad.adrelid AND a.attnum = ad.adnum
     JOIN (pg_class c JOIN pg_namespace nc ON c.relnamespace = nc.oid) ON a.attrelid = c.oid
     JOIN (pg_type t JOIN pg_namespace nt ON t.typnamespace = nt.oid) ON a.atttypid = t.oid
-    LEFT JOIN (pg_type bt JOIN pg_namespace nbt ON bt.typnamespace = nbt.oid) ON t.typtype = 'd' AND t.typbasetype = bt.oid
 WHERE NOT pg_is_other_temp_schema(nc.oid)
     AND a.attnum > 0
     AND NOT a.attisdropped
-    AND (c.relkind = ANY (ARRAY['r', 'v', 'f', 'p'])) 
-    AND (pg_has_role(c.relowner, 'USAGE') OR has_column_privilege(c.oid, a.attnum, 'SELECT, INSERT, UPDATE, REFERENCES'))
+    AND c.relkind = 'r'
     AND nc.nspname = ANY($1);
